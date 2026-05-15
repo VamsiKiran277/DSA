dbd55567723d42b1f3100228ccab35cca69789d5
Vamsi Kiran <vamsikiran167@gmail.com>
2026-05-15 13:25:21 +0100

    Insertion sort implementations

--- Insertion Sort_Descending Order/main.c
+++ Insertion Sort_Descending Order/main.c
@@ -13,7 +13,7 @@ void insertion_sort (int arr[],int length) {
 	for(int i=1; i<length; i++) {
 		int key = arr[i];
 		int j = i-1;
-		while(j>=0 && arr[j]>key) {
+		while(j>=0 && arr[j]<key) {
 			arr[j+1] = arr[j];
 			j = j-1;
 		}
